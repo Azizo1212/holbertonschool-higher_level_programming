1e17492722d8f56a2f4dbf55ceeb4a5fb62e8102
Azizo1212 <azizdali530@gmail.com>
2022-01-22 09:33:58 -0800

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
+class Square:
 """Private instance attribute: size
 Instantiation with size (no type/value verification)
 You are not allowed to import any module"""
 
 
-class Square:
     def __init__(self, size):
         self.__size = size
