6985652f5d78a2b44a2807fddc87fc928c311d32
Azizo1212 <azizdali530@gmail.com>
2022-02-01 10:05:06 -0800

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+"""Read file"""
 
 
 def read_file(filename=""):
+    """Read file"""
+
     with open(filename) as f:
         read_text = f.read()
         print(read_text, end="")
