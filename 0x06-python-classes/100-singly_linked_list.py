46caadff5f2cecc2ade487f1448a5f30c349a01a
Azizo1212 <azizdali530@gmail.com>
2022-02-01 09:07:11 -0800

    100-singly_linked_list.py

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -3,20 +3,17 @@
 
 
 class Node:
-    """Represents Node"""
-
+    """Node name of class"""
     def __init__(self, data, next_node=None):
-        self.__data = data
+        self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
-        """Retrieves data."""
         return self.__data
 
     @data.setter
     def data(self, value):
-
         if type(value) != int:
             raise TypeError("data must be an integer")
         else:
@@ -24,48 +21,43 @@ class Node:
 
     @property
     def next_node(self):
-
-        """ get next_node attribute"""
         return self.__next_node
 
-    @position.setter
+    @next_node.setter
     def next_node(self, value):
-        if type(value) != Node or type(value) is not None:
+        if value is not None and type(value) != Node:
             raise TypeError("next_node must be a Node object")
         else:
             self.__next_node = value
-
 
 """class SinglyLinkedList."""
 
 
 class SinglyLinkedList:
-    """SinglyLinkedList"""
-
+    """name of class singlylinkedlist"""
     def __init__(self):
         self.head = None
 
     def __str__(self):
-
         i = ""
         node = self.head
-        while node:
+        while node is not None:
             i += str(node.data)
             i += '\n'
             node = node.next_node
         return i[:-1]
 
     def sorted_insert(self, value):
-        new = Node(value)
+        new_node = Node(value)
         if self.head is None:
-            self.head = new
+            self.head = new_node
             return
         if value < self.head.data:
-            new.next_node = self.head
-            self.head = new
+            new_node.next_node = self.head
+            self.head = new_node
             return
         node = self.head
         while node.next_node and node.next_node.data < value:
             node = node.next_node
-        new.next_node = node.next_node
-        node.next_node = new
+        new_node.next_node = node.next_node
+        node.next_node = new_node
