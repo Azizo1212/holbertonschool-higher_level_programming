b69b29253fe46738eb5e105f31ddc757ccccf493
Azizo1212 <Azizdali530@gmail.com>
2022-05-20 19:58:38 +0200

    4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -14,9 +14,13 @@ if __name__ == '__main__':
             db=sys.argv[3])
 
     cursor = db.cursor()
-    cursor.execute("""SELECT  cities.id, cities.name, states.name
-                FROM cities INNER JOIN states ON cities.state_id=states.id""")
+    cursor.execute("SELECT  cities.id, cities.name, states.name
+                FROM cities INNER JOIN states ON cities.state_id=states.id", (sys.argv[4],)))
 
     rows = cursor.fetchall()
     for row in rows:
         print(row)
+    print(", ".join([row[0] for row in rows]))
+    cursor.close()
+    database.close()
+
