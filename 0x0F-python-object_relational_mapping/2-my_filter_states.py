1c6238c11ea1d5dd245985db54c60fd80eb595f9
Azizo1212 <Azizdali530@gmail.com>
2022-05-20 17:50:37 +0200

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,6 +12,7 @@ if __name__ == '__main__':
             user=sys.argv[1],
             passwd=sys.argv[2],
             db=sys.argv[3],
+            name_searched=sys.argv[4]
 
     cursor = db.cursor()
     cursor.execute("""SELECT * FROM states WHERE states.name LIKE '%{:s}%'
