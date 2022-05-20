2b9f1a80d6b5d7bfafd16f72c5784f63093277d9
Azizo1212 <Azizdali530@gmail.com>
2022-05-20 17:31:13 +0200

    2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""lists all states from the database hbtn_0e_0_usa"""
+
+if __name__ == '__main__':
+
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            state=sys.argv[4])
+
+    cursor = db.cursor()
+    cursor.execute("""SELECT * FROM states WHERE states.name LIKE '%{:s}%'
+                   ORDER BY states.id ASC""".format(state))
+
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
