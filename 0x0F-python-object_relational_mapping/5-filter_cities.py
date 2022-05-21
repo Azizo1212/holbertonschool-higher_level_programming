ef455623dea87d6334ebcc28c894ba6026d8c0c6
Azizo1212 <Azizdali530@gmail.com>
2022-05-21 16:16:59 +0200

    5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     curso.execute("SELECT cities.name FROM cities\
                 INNER JOIN states ON cities.state_id = states.id\
                 WHERE states.name = %s ORDER BY cities.id",
-                (sys.argv[4], ))
+                (argv[4], ))
 
     rows = curso.fetchall()
     new = 0
