0e9cd8b0ef2a24192b287b86c4ac619538ea30ed
Azizo1212 <Azizdali530@gmail.com>
2022-05-22 18:05:56 +0200

    5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,31 +1,27 @@
 #!/usr/bin/python3
-"""lists all cities of that stateusing the database hbtn_0e_4_usa"""
+"""takes in the name of a state as an argument and lists all cities of that stat"""
 
-if __name__ == '__main__':
 
+if __name__ == "__main__":
+    from sys import argv
     import MySQLdb
-    import sys
-
-    db = MySQLdb.connect(host='localhost'
-                         port=3306,
-                         user=sys.argv[1],
-                         passwd=sys.argv[2],
-                         db=sys.argv[3])
-
+    db = MySQLdb.connect(user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
     cursor = db.cursor()
+
     cursor.execute("SELECT cities.name FROM cities\
                 INNER JOIN states ON cities.state_id = states.id\
-                WHERE states.name = %s ORDER BY cities.id ASC",(argv[4], ))
+                WHERE states.name = %s ORDER BY cities.id",
+                (argv[4], ))
 
-    rows = cursor.fetchall()
-    new = 0
-
+    row = cursor.fetchall()
+    i = 0
     for woof in row:
-        if new != 0:
+        if i != 0:
             print(", ", end="")
         print("%s" % woof, end="")
-        new = new + 1
+        i = i + 1
     print("")
-
     cursor.close()
     db.close()
