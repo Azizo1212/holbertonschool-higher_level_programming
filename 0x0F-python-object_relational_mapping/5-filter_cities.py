b168bdd079256ef001540e1b36e5191bcf205a42
Azizo1212 <Azizdali530@gmail.com>
2022-05-23 17:08:55 +0200

    5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """takes in the name of a state as an argument and lists all cities of that stat"""
 
+
 if __name__ == "__main__":
+
     from sys import argv
     import MySQLdb
     db = MySQLdb.connect(user=argv[1],passwd=argv[2],db=argv[3])
