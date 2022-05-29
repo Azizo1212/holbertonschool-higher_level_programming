95989a6ed1c87eae4fb4ac20d69968cc03c29d7f
Azizo1212 <Azizdali530@gmail.com>
2022-05-29 19:56:52 +0200

    7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 
+#print c is fun as many as argv[2]
 const myVar = 'C is fun';
 if (isNaN(process.argv[2])) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < process.argv[2]; i++) {
+  for (let i = 1; i <= process.argv[2]; i++) {
     console.log(myVar);
   }
 }
