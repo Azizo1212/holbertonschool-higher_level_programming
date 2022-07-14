612ed9f10eec32fd1addca02f27a1e8273b6d6e8
Azizo1212 <azizdali530@gmail.com>
2022-07-14 02:39:57 +0200

    8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user"""
+
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    if (len(sys.argv) > 1):
+        q = sys.argv[1]
+    else:
+        q = ""
+    response = requests.post("http://0.0.0.0:5000/search_user", data={'q': q})
+    try:
+        jresponse = response.json()
+        if jresponse == {}:
+            print("No result")
+        else:
+            ___id = jresponse['id']
+            ___name = jresponse['name']
+            print("[{}] {}".format(___id, ___name))
+    except Exception:
+        print("Not a valid JSON")
