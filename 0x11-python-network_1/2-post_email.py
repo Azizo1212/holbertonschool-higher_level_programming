caac6e3e71a2f465aa9fc26b0d3bcdd0cdf82325
Azizo1212 <azizdali530@gmail.com>
2022-07-14 02:05:53 +0200

    3-error_code.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """takes in a URL and an email"""
 
-if __name__ == "__main__":
 
-import urllib.request
-import urllib.parse
-import sys
+from urllib import request, parse
+from sys import argv
 
+
+if __name__ == '__main__':
     url = argv[1]
-    value = {"email": sys.argv[2]}
-    data = parse.urlencode({'email': email}).encode()
-    str = request.Request(url=url, data=data)
-
-    with request.urlopen(str) as response:
+    email = argv[2]
+    data = parse.urlencode({'email': email})
+    data = data.encode("ascii")
+    req = request.Request(url, data)
+    with request.urlopen(req) as response:
         print(response.read().decode('utf-8'))
---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" takes in a URL, sends a request to the URL and displays the body of the response """
+
+from urllib import request
+from urllib.error import HTTPError
+from sys import argv
+
+
+if __name__ == '__main__':
+    url = argv[1]
+    try:
+        with request.urlopen(url) as response:
+            print(response.read().decode('utf-8'))
+    except HTTPError as e:
+            print("Error code: {}".format(e.code))
