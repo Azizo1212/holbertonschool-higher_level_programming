2269fe44746e8c36d144dcd903ec5a9e404fc15c
Azizo1212 <azizdali530@gmail.com>
2022-12-16 17:36:27 +0100

    6-completed_tasks.js

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
+
+  if (!error) {
+    const todos = JSON.parse(body);
+    const t1 = {};
+    todos.forEach((todo) => {
+      if (todo.completed && t1[todo.userId] === undefined) {
+        t1[todo.userId] = 1;
+      } else if (todo.completed) {
+        t1[todo.userId] += 1;
+      }
+    });
+    console.log(t1);
+  }
+})
